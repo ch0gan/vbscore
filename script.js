b73149b24ec198e8ed7ede423b64525391077691
Sean Hogan <157655532+Shogan2@users.noreply.github.com>
2025-06-25 23:42:44 -0400

    Included saveState and loadState and various fixes
    Listed Fixes:
    
    Menu Changes:
    - Can now exit menu by clicking anywhere outside of the menu box as well as the X in the top right of the menu
    - Increased the width of the menu
    - Increased the height of the Side Out vs Rally drop down menu
    - Increased the height, font, and appearance of team name text boxes
    - made the Reset button smaller
    - When on mobile, you can scroll down on the menu if you can't see the whole thing in landscape mode
    
    Save/Load States (things that remain saved between refreshing the website):
    - Score when increased on main page
    - Score when increased/decreased in menu
    - Score when reset in menu
    - Team Names
    - Lines under the last team name/ score who scored
    - Side Out vs Rally Scoring
    - The side that each team was on

--- script.js
+++ script.js
@@ -9,15 +9,29 @@ let teamAName = "Team 1";
 let teamBName = "Team 2";
 //let servingTeam = null; /* 'A';   null for now and assumes we don't know who is serving */
 let strScoreType = document.getElementById('scoringType').value;
+let isSwitched = false; // used to track if the teams have been switched
 
 
 function updateTeamNames() {
-   //document.querySelector('input[name="team1"]').innerHTML    = teamAName;
    document.getElementsByName('team1')[0].innerHTML = teamAName;
    document.getElementsByName('team2')[0].innerHTML = teamBName;
 }
 // set the default team names using the global variables
 updateTeamNames();
+
+function updateTeamAName() {
+    const input = document.getElementById('team1');
+    teamAName = input.value || 'Team 1';
+    updateTeamNames();
+    saveState();
+}
+
+function updateTeamBName() {
+    const input = document.getElementById('team2');
+    teamBName = input.value || 'Team 2';
+    updateTeamNames();
+    saveState();
+}
 
 
 function setScoring() {
@@ -67,6 +81,13 @@ function increaseScore(team) {
            servingTeam = teamB_serve;
         }
    }
+
+   // Set focus to the correct button to trigger :focus underline
+   if (team === 'A') {
+       document.getElementById('scoreA').focus();
+   } else if (team === 'B') {
+       document.getElementById('scoreB').focus();
+   }
   
    //if (team === 'A') {
    //    scoreA++;
@@ -82,8 +103,8 @@ function increaseScore(team) {
    //    servingTeam = 'B'; /* used for sideout */
    //}
    //console.log(servingTeam);
-
-
+   
+    saveState();
 }
 
 
@@ -119,6 +140,7 @@ function resetScores() {
    document.getElementById('adjustScoreA').innerText = scoreA;
    document.getElementById('adjustScoreB').innerText = scoreB;
    servingTeam = null;
+   saveState();
 }
 
 
@@ -133,12 +155,15 @@ function adjustScore(team, amount) {
        document.querySelector('#scoreB span').innerText = scoreB;
        document.getElementById('adjustScoreB').innerText = scoreB;
    }
+   saveState();
 }
 
 
 function switchDivs() {
    let container = document.getElementsByClassName('container')[0];
    container.appendChild(container.children[0]);
+   isSwitched = !isSwitched;
+   saveState();
    //console.log('switchted!');
   
    // this section is needed to make the underline appear on the right button after the switch
@@ -150,5 +175,61 @@ function switchDivs() {
        document.getElementById("scoreB").focus();
        //console.log("should be B - " + servingTeam);
    }
+   saveState();
 }
 
+// Allow closing menu by clicking outside the menu content
+document.getElementById('menuOverlay').addEventListener('click', function(event) {
+    if (event.target === this) {
+        toggleMenu();
+    }
+});
+
+
+// Load saved state from localStorage
+function saveState() {
+    localStorage.setItem('vbscore_state', JSON.stringify({
+        scoreA,
+        scoreB,
+        teamAName,
+        teamBName,
+        servingTeam,
+        scoringType: document.getElementById('scoringType').value,
+        isSwitched
+    }));
+}
+
+document.getElementById('scoringType').addEventListener('change', saveState);
+
+function loadState() {
+    const state = JSON.parse(localStorage.getItem('vbscore_state'));
+    if (state) {
+        scoreA = state.scoreA || 0;
+        scoreB = state.scoreB || 0;
+        teamAName = state.teamAName || "Team 1";
+        teamBName = state.teamBName || "Team 2";
+        servingTeam = state.servingTeam || 0;
+        document.getElementById('scoringType').value = state.scoringType || 'sideout';
+        isSwitched = state.isSwitched || false;
+    }
+}
+
+loadState();
+updateTeamNames();
+document.querySelector('#scoreA span').innerText = scoreA;
+document.querySelector('#scoreB span').innerText = scoreB;
+document.getElementById('adjustScoreA').innerText = scoreA;
+document.getElementById('adjustScoreB').innerText = scoreB;
+
+// Restore underline to the last serving team after loading state
+if (servingTeam == teamA_serve) {
+    document.getElementById("scoreA").focus();
+} else if (servingTeam == teamB_serve) {
+    document.getElementById("scoreB").focus();
+}
+
+if (isSwitched == true) {
+    switchDivs();
+    isSwitched = true;
+    saveState();
+}
